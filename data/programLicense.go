01d06da391b8715a895305f0675cb9144cca24fc
JanFan <termal12345@gmail.com>
2020-04-02 23:39:48 +0600

    чтение / запись токена из файла проверка...

--- data/programLicense.go
+++ data/programLicense.go
@@ -1,8 +1,11 @@
 package data
 
 import (
+	"github.com/JanFant/TLServer/logger"
 	u "github.com/JanFant/TLServer/utils"
 	"github.com/dgrijalva/jwt-go"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
@@ -19,15 +22,14 @@ type LicenseToken struct {
 
 //License информация о лицензии клиента (БД?)
 type License struct {
-	NumDevice        int       `json:"numDev"`           //количество устройств
-	NameClient       string    `json:"name"`             //название фирмы
-	AddressClient    string    `json:"address"`          //адресс фирмы
-	PhoneClient      string    `json:"phone"`            //телефон фирмы
-	EmailClient      string    `json:"email"`            //емайл фирмы
-	YaKey            string    `json:"yaKey"`            //ключ яндекса
-	TokenPass        string    `json:"tokenPass"`        //пароль для шифрования токена https запросов
-	EndTime          time.Time `json:"time"`             //время окончания лицензии
-	LicenseTokenPass string    `json:"LicenseTokenPass"` //пароль шифрования токена лицензии
+	NumDevice     int       `json:"numDev"`    //количество устройств
+	NameClient    string    `json:"name"`      //название фирмы
+	AddressClient string    `json:"address"`   //адресс фирмы
+	PhoneClient   string    `json:"phone"`     //телефон фирмы
+	EmailClient   string    `json:"email"`     //емайл фирмы
+	YaKey         string    `json:"yaKey"`     //ключ яндекса
+	TokenPass     string    `json:"tokenPass"` //пароль для шифрования токена https запросов
+	EndTime       time.Time `json:"time"`      //время окончания лицензии
 }
 
 func CreateLicenseToken(license License) map[string]interface{} {
@@ -39,7 +41,7 @@ func CreateLicenseToken(license License) map[string]interface{} {
 	tk.ExpiresAt = license.EndTime.Unix()
 
 	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
-	tokenString, _ := token.SignedString([]byte(license.LicenseTokenPass))
+	tokenString, _ := token.SignedString([]byte(GlobalConfig.TokenPassword))
 
 	//сохраняем токен в БД
 	//GetDB().Exec("update public.accounts set token = ? where login = ?", account.Token, account.Login)
@@ -49,4 +51,40 @@ func CreateLicenseToken(license License) map[string]interface{} {
 	resp["token"] = tokenString
 	resp["license"] = license
 	return resp
+}
+
+func CheckLicenseKey(tokenSTR string) {
+	tk := &LicenseToken{}
+	token, err := jwt.ParseWithClaims(tokenSTR, tk, func(token *jwt.Token) (interface{}, error) {
+		return []byte(GlobalConfig.TokenPassword), nil
+	})
+
+	//не правильный токен
+	if err != nil {
+	}
+	//не истек ли токен?
+	if !token.Valid {
+
+	}
+}
+
+func ControlLicenseKey() {
+}
+
+func readFile() (string, error) {
+	byteFile, err := ioutil.ReadFile("license.key")
+	if err != nil {
+		logger.Error.Println("|Message: Error reading license.key file")
+		return "", err
+	}
+	return string(byteFile), nil
+}
+
+func writeFile(tokenStr string) error {
+	err := ioutil.WriteFile("license.key", []byte(tokenStr), os.ModePerm)
+	if err != nil {
+		logger.Error.Println("|Message: Error write license.key file")
+		return err
+	}
+	return nil
 }
