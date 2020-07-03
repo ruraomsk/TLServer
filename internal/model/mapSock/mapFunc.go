53f9407645762a99a3099f3b44533e9e45f75fee
JanFan <termal12345@gmail.com>
2020-07-03 09:48:01 +0600

    утащил сокет карты в отделный каталог

---
+++ internal/model/mapSock/mapFunc.go
@@ -0,0 +1,171 @@
+package mapSocket
+
+import (
+	"fmt"
+	"github.com/JanFant/TLServer/internal/model/data"
+	"github.com/JanFant/TLServer/internal/model/license"
+	"github.com/JanFant/TLServer/logger"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/jmoiron/sqlx"
+	"golang.org/x/crypto/bcrypt"
+	"strings"
+	"time"
+)
+
+//login обработчик авторизации пользователя в системе
+func Login(login, password, ip string, db *sqlx.DB) MapSokResponse {
+	ipSplit := strings.Split(ip, ":")
+	account := &data.Account{}
+	//Забираю из базы запись с подходящей почтой
+	rows, err := db.Query(`SELECT id, login, password, work_time, description FROM public.accounts WHERE login=$1`, login)
+	if rows == nil {
+		resp := newMapMess(typeError, nil, nil)
+		resp.Data["message"] = fmt.Sprintf("Invalid login credentials. login(%s)", login)
+		return resp
+	}
+	if err != nil {
+		resp := newMapMess(typeError, nil, nil)
+		resp.Data["message"] = "connection to DB error. Please try again"
+		return resp
+	}
+	for rows.Next() {
+		_ = rows.Scan(&account.ID, &account.Login, &account.Password, &account.WorkTime, &account.Description)
+	}
+
+	//Авторизировались добираем полномочия
+	privilege := data.Privilege{}
+	err = privilege.ReadFromBD(account.Login)
+	if err != nil {
+		resp := newMapMess(typeError, nil, nil)
+		resp.Data["message"] = fmt.Sprintf("Invalid login credentials. login(%s)", login)
+		return resp
+	}
+
+	//Сравниваю хэши полученного пароля и пароля взятого из БД
+	err = bcrypt.CompareHashAndPassword([]byte(account.Password), []byte(password))
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+		resp := newMapMess(typeError, nil, nil)
+		resp.Data["message"] = fmt.Sprintf("Invalid login credentials. login(%s)", account.Login)
+		return resp
+	}
+	//Залогинились, создаем токен
+	account.Password = ""
+	tk := &data.Token{
+		UserID:      account.ID,
+		Login:       account.Login,
+		IP:          ipSplit[0],
+		Role:        privilege.Role.Name,
+		Region:      privilege.Region,
+		Area:        privilege.Area,
+		Permission:  privilege.Role.Perm,
+		Description: account.Description,
+	}
+	//врямя выдачи токена
+	tk.IssuedAt = time.Now().Unix()
+	//время когда закончится действие токена
+	tk.ExpiresAt = time.Now().Add(time.Hour * account.WorkTime).Unix()
+
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	tokenString, _ := token.SignedString([]byte(license.LicenseFields.TokenPass))
+	account.Token = tokenString
+	//сохраняем токен в БД чтобы точно знать что дейтвителен только 1 токен
+
+	_, err = db.Exec(`UPDATE public.accounts SET token = $1 WHERE login = $2`, account.Token, account.Login)
+	if err != nil {
+		resp := newMapMess(typeError, nil, nil)
+		resp.Data["message"] = "connection to DB error. Please try again"
+		return resp
+	}
+
+	//Формируем ответ
+	resp := newMapMess(typeLogin, nil, nil)
+	resp.Data["login"] = account.Login
+	resp.Data["token"] = tokenString
+	resp.Data["manageFlag"], _ = data.AccessCheck(login, privilege.Role.Name, 2)
+	resp.Data["logDeviceFlag"], _ = data.AccessCheck(login, privilege.Role.Name, 5)
+	resp.Data["techArmFlag"], _ = data.AccessCheck(login, privilege.Role.Name, 7)
+	resp.Data["authorizedFlag"] = true
+	resp.Data["description"] = account.Description
+	resp.Data["region"] = privilege.Region
+	//собрать в районы с их названиями
+	var areaMap = make(map[string]string)
+	for _, area := range privilege.Area {
+		var tempA data.AreaInfo
+		tempA.SetAreaInfo(privilege.Region, area)
+		areaMap[tempA.Num] = tempA.NameArea
+	}
+	resp.Data["area"] = areaMap
+
+	data.CacheArea.Mux.Lock()
+	resp.Data["areaBox"] = data.CacheArea.Areas
+	data.CacheArea.Mux.Unlock()
+	return resp
+}
+
+//LogOut выход из учетной записи
+func LogOut(login string, db *sqlx.DB) MapSokResponse {
+	_, err := db.Exec("UPDATE public.accounts SET token = $1 where login = $2", "", login)
+	if err != nil {
+		resp := newMapMess(typeError, nil, nil)
+		resp.Data["message"] = "connection to DB error. Please try again"
+		return resp
+	}
+	return newMapMess(typeLogOut, nil, nil)
+}
+
+//SelectTL возвращает массив в котором содержатся светофоры, которые попали в указанную область
+func SelectTL(db *sqlx.DB) (tfdata []data.TrafficLights) {
+	var dgis string
+	temp := &data.TrafficLights{}
+	rowsTL, err := db.Query(`SELECT region, area, subarea, id, idevice, dgis, describ, status FROM public.cross`)
+	if err != nil {
+		logger.Error.Println("|Message: db not respond", err.Error())
+		return nil
+	}
+	for rowsTL.Next() {
+		err := rowsTL.Scan(&temp.Region.Num, &temp.Area.Num, &temp.Subarea, &temp.ID, &temp.Idevice, &dgis, &temp.Description, &temp.Sost.Num)
+		if err != nil {
+			logger.Error.Println("|Message: No result at these points", err.Error())
+			return nil
+		}
+		temp.Points.StrToFloat(dgis)
+		data.CacheInfo.Mux.Lock()
+		temp.Region.NameRegion = data.CacheInfo.MapRegion[temp.Region.Num]
+		temp.Area.NameArea = data.CacheInfo.MapArea[temp.Region.NameRegion][temp.Area.Num]
+		temp.Sost.Description = data.CacheInfo.MapTLSost[temp.Sost.Num]
+		data.CacheInfo.Mux.Unlock()
+		tfdata = append(tfdata, *temp)
+	}
+
+	return tfdata
+}
+
+func MapOpenInfo(db *sqlx.DB) (obj map[string]interface{}) {
+	obj = make(map[string]interface{})
+
+	location := &data.Locations{}
+	box, _ := location.MakeBoxPoint()
+	obj["boxPoint"] = &box
+	obj["tflight"] = SelectTL(db)
+	obj["authorizedFlag"] = false
+
+	//собираю в кучу регионы для отображения
+	chosenRegion := make(map[string]string)
+	data.CacheInfo.Mux.Lock()
+	for first, second := range data.CacheInfo.MapRegion {
+		chosenRegion[first] = second
+	}
+	delete(chosenRegion, "*")
+	obj["regionInfo"] = chosenRegion
+
+	//собираю в кучу районы для отображения
+	chosenArea := make(map[string]map[string]string)
+	for first, second := range data.CacheInfo.MapArea {
+		chosenArea[first] = make(map[string]string)
+		chosenArea[first] = second
+	}
+	delete(chosenArea, "Все регионы")
+	data.CacheInfo.Mux.Unlock()
+	obj["areaInfo"] = chosenArea
+	return
+}
---
+++ internal/model/mapSock/mapSocket.go
@@ -0,0 +1,182 @@
+package mapSocket
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/JanFant/TLServer/internal/model/chat"
+	"github.com/JanFant/TLServer/internal/model/config"
+	"github.com/JanFant/TLServer/internal/model/data"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"github.com/jmoiron/sqlx"
+	"time"
+)
+
+var connectedUsersOnMap map[*websocket.Conn]bool
+var writeMap chan MapSokResponse
+
+const pingPeriod = time.Second * 30
+
+//MapReader обработчик открытия сокета для карты
+func MapReader(conn *websocket.Conn, c *gin.Context, db *sqlx.DB) {
+	connectedUsersOnMap[conn] = true
+	login := ""
+	{
+		flag, tk := checkToken(c, db)
+		resp := newMapMess(typeMapInfo, conn, MapOpenInfo(db))
+		if flag {
+			login = tk.Login
+			role := tk.Role
+			resp.Data["manageFlag"], _ = data.AccessCheck(login, role, 2)
+			resp.Data["logDeviceFlag"], _ = data.AccessCheck(login, role, 5)
+			resp.Data["techArmFlag"], _ = data.AccessCheck(login, role, 7)
+			resp.Data["description"] = tk.Description
+			resp.Data["authorizedFlag"] = true
+			resp.Data["region"] = tk.Region
+			var areaMap = make(map[string]string)
+			for _, area := range tk.Area {
+				var tempA data.AreaInfo
+				tempA.SetAreaInfo(tk.Region, area)
+				areaMap[tempA.Num] = tempA.NameArea
+			}
+			resp.Data["area"] = areaMap
+			data.CacheArea.Mux.Lock()
+			resp.Data["areaBox"] = data.CacheArea.Areas
+			data.CacheArea.Mux.Unlock()
+		}
+		resp.send()
+	}
+	data.GetCrossUserForMap <- true
+	for {
+		_, p, err := conn.ReadMessage()
+		if err != nil {
+			//закрытие коннекта
+			resp := newMapMess(typeClose, conn, nil)
+			resp.send()
+			return
+		}
+
+		typeSelect, err := setTypeMessage(p)
+		if err != nil {
+			resp := newMapMess(typeError, conn, nil)
+			resp.Data["message"] = ErrorMessage{Error: errUnregisteredMessageType}
+			resp.send()
+		}
+		switch typeSelect {
+		case typeJump:
+			{
+				location := &data.Locations{}
+				_ = json.Unmarshal(p, &location)
+				box, _ := location.MakeBoxPoint()
+				resp := newMapMess(typeJump, conn, nil)
+				resp.Data["boxPoint"] = box
+				resp.send()
+			}
+		case typeLogin:
+			{
+				account := &data.Account{}
+				_ = json.Unmarshal(p, &account)
+				resp := newMapMess(typeLogin, conn, nil)
+				resp = Login(account.Login, account.Password, conn.RemoteAddr().String(), db)
+				if resp.Type == typeLogin {
+					login = fmt.Sprint(resp.Data["login"])
+				}
+				resp.conn = conn
+				resp.send()
+			}
+		case typeLogOut:
+			{
+				if login != "" {
+					resp := LogOut(login, db)
+					resp.conn = conn
+					resp.Data["authorizedFlag"] = true
+					resp.send()
+					chat.UserLogoutChat <- login
+				}
+			}
+
+		}
+	}
+}
+
+//MapBroadcast передатчик для карты (map)
+func MapBroadcast(db *sqlx.DB) {
+	connectedUsersOnMap = make(map[*websocket.Conn]bool)
+	writeMap = make(chan MapSokResponse)
+
+	crossReadTick := time.NewTicker(time.Second * 5)
+	pingTicker := time.NewTicker(pingPeriod)
+
+	defer func() {
+		pingTicker.Stop()
+		crossReadTick.Stop()
+	}()
+	oldTFs := SelectTL(db)
+	for {
+		select {
+		case <-crossReadTick.C:
+			{
+				if len(connectedUsersOnMap) > 0 {
+					newTFs := SelectTL(db)
+					var tempTF []data.TrafficLights
+					for _, nTF := range newTFs {
+						for _, oTF := range oldTFs {
+							if oTF.Idevice == nTF.Idevice && oTF.Sost.Num != nTF.Sost.Num {
+								tempTF = append(tempTF, nTF)
+								break
+							}
+						}
+					}
+					oldTFs = newTFs
+					if len(tempTF) > 0 {
+						resp := newMapMess(typeTFlight, nil, nil)
+						resp.Data["tflight"] = tempTF
+						for conn := range connectedUsersOnMap {
+							_ = conn.WriteJSON(resp)
+						}
+					}
+				}
+			}
+		case <-data.MapRepaint:
+			{
+				time.Sleep(time.Second * time.Duration(config.GlobalConfig.DBConfig.DBWait))
+				oldTFs = SelectTL(db)
+				resp := newMapMess(typeRepaint, nil, nil)
+				resp.Data["tflight"] = oldTFs
+				data.FillMapAreaBox()
+				data.CacheArea.Mux.Lock()
+				resp.Data["areaBox"] = data.CacheArea.Areas
+				data.CacheArea.Mux.Unlock()
+				for conn := range connectedUsersOnMap {
+					_ = conn.WriteJSON(resp)
+				}
+
+			}
+		case <-pingTicker.C:
+			{
+				for conn := range connectedUsersOnMap {
+					_ = conn.WriteMessage(websocket.PingMessage, nil)
+				}
+			}
+		case crossUsers := <-data.CrossUsersForMap:
+			{
+				resp := newMapMess(typeEditCrossUsers, nil, nil)
+				resp.Data["editCrossUsers"] = crossUsers
+				for conn := range connectedUsersOnMap {
+					_ = conn.WriteJSON(resp)
+				}
+			}
+		case msg := <-writeMap:
+			switch msg.Type {
+			case typeClose:
+				{
+					delete(connectedUsersOnMap, msg.conn)
+				}
+			default:
+				{
+					_ = msg.conn.WriteJSON(msg)
+				}
+			}
+		}
+	}
+}
